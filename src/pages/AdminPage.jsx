1344418579b66a3db174a5a935641f09bebe39e3
Fredrik Olsvold <fredrik.olsvold@gmail.com>
2021-03-24 10:03:23 +0100

    nothing was added? strange merge conflicts

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,21 +1,8 @@
 import React from "react";
-<<<<<<< HEAD
 import AdminGameList from "../components/admin-pages/admin-gameCard/AdminGameList";
 
 function AdminPage() {
   return <AdminGameList />;
-=======
-import AdminDashboard from "../components/admin-pages/admin-dashboard/AdminDashboard";
-import AdminForm from "../components/admin-pages/AdminForm";
-import Main from "../components/main/Main";
-
-function AdminPage() {
-  return (
-    <Main>
-      <AdminDashboard />
-    </Main>
-  );
->>>>>>> hvz-69-login-register-form
 }
 
 export default AdminPage;
