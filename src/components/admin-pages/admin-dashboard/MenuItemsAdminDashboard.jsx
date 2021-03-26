e4ef77ad65c13873070fd3b2e49121121d31707c
Sofie Bergqvist <sofie.bergqvist@no.experis.com>
2021-03-26 09:18:59 +0100

    Update menu and split up menuitems to components

--- src/components/admin-pages/admin-dashboard/MenuItemsAdminDashboard.jsx
+++ src/components/admin-pages/admin-dashboard/MenuItemsAdminDashboard.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useState } from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import PeopleIcon from "@material-ui/icons/People";
 import { AddLocation, Delete, HomeRounded, PostAdd } from "@material-ui/icons";
 import { Cloudinary } from "cloudinary-core";
@@ -13,47 +12,12 @@ import MenuIcon_ThemeToggle from "../../menu-items/MenuIcon_ThemeToggle";
 import { Divider } from "@material-ui/core";
 
 export default function MenuItemsAdminDashboard({ games }) {
-  const useStyles = makeStyles((theme) => ({
-    registrationButton: {
-      marginRight: 36,
-    },
-    registrationButtonHidden: {
-      display: "none",
-    },
-    gameButton: {
-      marginRight: 36,
-    },
-    gameButtonHidden: {
-      display: "none",
-    },
-    customWidth: {
-      maxWidth: 120,
-    },
-  }));
-  const classes = useStyles();
-
   const [open, setOpen] = useState(false);
   const history = useHistory();
-  //Fetch game
-  // const { data: games, error: gamesError } = useSWR(
-  //   `${Endpoints.GAME_API}`,
-  //   (url) => fetcherToken(url, getTokenInStorage())
-  // );
-  // console.log("games", games);
-  // console.log("hej");
-  // console.log(games.id);
+
   console.log(games);
 
-  const onClickDelete = () => {
-    // fetch(`${Endpoints.GAME_API}/${game.id}`, {
-    //   method: "DELETE",
-    //   headers: {
-    //     Authorization: "Bearer " + getTokenInStorage(),
-    //     "Content-Type": "application/json",
-    //   },
-    //   body: JSON.stringify(data),
-    // }).then((res) => res.json().then((res) => console.warn("result", res)));
-  };
+  const onClickDelete = () => {};
 
   const cloudinaryCore = new Cloudinary({ cloud_name: "debyqnalg" });
 
--- src/components/menu-items/MenuItem_StartGame.jsx
+++ src/components/menu-items/MenuItem_StartGame.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   ListItem,
   ListItemIcon,
@@ -8,8 +8,13 @@ import {
 } from "@material-ui/core";
 import { HighlightOff, PlayCircleOutline } from "@material-ui/icons";
 import DialogPopUp from "../admin-pages/admin-dashboard/DialogPopUp";
+import { fetcherToken } from "../../services/FetcherFunction";
+import useSWR from "swr";
+import Endpoints from "../../services/endpoints";
+import { getTokenInStorage } from "../../utils/tokenHelper";
+import { useLocation, useHistory } from "react-router-dom";
 
-const MenuItem_StartGame = () => {
+const MenuItem_StartGame = (props) => {
   //Text constants for Buttons and Dialog
   const gameStart = "Start Game";
   const gameEnd = "End Game";
@@ -37,6 +42,54 @@ const MenuItem_StartGame = () => {
     setopenPopUp(false); // close popup
     setGameState(gameState === true ? false : true);
   };
+  const location = useLocation();
+
+  const history = useHistory();
+  const [game, setGame] = useState({});
+
+  console.log(location.state);
+
+  useEffect(() => {
+    setGame(location.state);
+  }, [location.state]);
+
+  //Fetch game
+  // const { data: games, error: gamesError } = useSWR(
+  //   `${Endpoints.GAME_API}`,
+  //   (url) => fetcherToken(url, getTokenInStorage())
+  // );
+  // console.log("games", games);
+  // console.log("hej");
+  // console.log(games.id);
+
+  // fetch(`${Endpoints.GAME_API}/${game.id}`, {
+  //   method: "DELETE",
+  //   headers: {
+  //     Authorization: "Bearer " + getTokenInStorage(),
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify(data),
+  // }).then((res) => res.json().then((res) => console.warn("result", res)));
+
+  //if game not started
+  const gameHasStarted = () => {
+    if (game.started) {
+      setGameState(true);
+      console.log("startet", game);
+    } else {
+      setGameState(false);
+    }
+  };
+
+  useEffect(() => {
+    if (game.started) {
+      setGameState(true);
+      console.log("startet", game.gameStarted);
+    } else {
+      setGameState(false);
+      console.log("startet", game.gameStarted);
+    }
+  }, []);
 
   return (
     <div>
@@ -61,8 +114,12 @@ const MenuItem_StartGame = () => {
         <Tooltip
           arrow
           placement={"bottom"}
-          aria-label={gameState ? `${"game has not started"}` : `${"end game"}`}
-          title={gameState ? `${"Game has not started"}` : `${"End game"}`}
+          aria-label={
+            !game.gameStarted ? `${"game has not started"}` : `${"end game"}`
+          }
+          title={
+            !game.gameStarted ? `${"Game has not started"}` : `${"End game"}`
+          }
         >
           <ListItem
             button
@@ -70,9 +127,11 @@ const MenuItem_StartGame = () => {
             aria-label="start game"
           >
             <ListItemIcon>
-              {gameState ? <PlayCircleOutline /> : <HighlightOff />}
+              {!game.gameStarted ? <PlayCircleOutline /> : <HighlightOff />}
             </ListItemIcon>
-            <ListItemText primary={gameState ? `${gameStart}` : `${gameEnd}`} />
+            <ListItemText
+              primary={!game.gameStarted ? `${gameStart}` : `${gameEnd}`}
+            />
           </ListItem>
         </Tooltip>
       </article>
