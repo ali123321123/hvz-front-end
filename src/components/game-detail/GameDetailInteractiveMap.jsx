50c0c96d9be06befa2c1d3dcbc941049f666dff2
Fredrik Olsvold <fredrik.olsvold@gmail.com>
2021-04-05 14:21:33 +0200

    added customMarkerIcon function to icon props in some Marker components

--- src/components/game-detail/GameDetailInteractiveMap.jsx
+++ src/components/game-detail/GameDetailInteractiveMap.jsx
@@ -3,6 +3,7 @@ import Map from "../map/Map";
 import { Marker, Popup, Rectangle } from "react-leaflet";
 import MapAddMarker from "../map/MapAddMarker";
 import Icons from "../../utils/icons";
+import { customMarkerIcon } from "../map/MapIconMaker";
 
 function GameDetailInteractiveMap({ playAreaCoordinates, kills }) {
   const [centerArea, setCenterArea] = useState(null);
@@ -19,7 +20,7 @@ function GameDetailInteractiveMap({ playAreaCoordinates, kills }) {
         <Map center={centerArea} zoom={17} scrollWheelZoom={true}>
           <Rectangle bounds={playAreaCoordinates} />
           {kills?.map((k) => (
-              <Marker icon={Icons.tombstone} position={[k.lat, k.lng]}>
+              <Marker icon={customMarkerIcon(Icons.tombstone)} position={[k.lat, k.lng]}>
                   <Popup>
                       {k.story}
                   </Popup>
